8c6296ef9f21e80d4c9f4a237f66c90e848398ec
chamberrr2809 <93575578+chamberrr2809@users.noreply.github.com>
2022-10-06 11:31:20 +0700

    selesai

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import nProgress from "nprogress";
+import "nprogress/nprogress.css";
+import Router from "next/router";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  Router.events.on("routeChangeStart", function () {
+    nProgress.start();
+  });
+  Router.events.on("routeChangeComplete", function () {
+    nProgress.done();
+  });
+  Router.events.on("routeChangeError", function () {
+    nProgress.done();
+  });
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "media",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/**/*.js",
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
+};
